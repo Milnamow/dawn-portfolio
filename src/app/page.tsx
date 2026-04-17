94a3a963a9a3123eab74bbf1a686ce6c414b93a8
Dawn Milnamow <dawnmil.zcw.de@gmail.com>
2026-04-17 16:21:42 -0400

    cleanup, spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import HeroVideo from "@/components/HeroVideo";
-import { ProfileAvatar } from "@/components/ProfileAvatar";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col gap-6 items-center justify-between text-center py-0 px-16 bg-white dark:bg-black">        
+      <main className="flex min-h-screen w-full max-w-3xl flex-col gap-6 items-center justify-between text-center py-32 px-16 bg-white dark:bg-black">        
         <HeroVideo />
         <h1 className="text-4xl font-bold">Dawn Milnamow</h1>
         <div>
